46db75c5f7c0702629d3ff52decbcd97876d749b
SevgiZ <christurner2384@gmail.com>
2026-03-08 15:50:46 +1100

    colour scheme

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const viewportWidth = window.innerWidth;
-const viewportHeight = window.innerHeight;
 
-positionElementsInCircle('.container-icons', 250)
 
-function positionElementsInCircle(containerName, radius) {
-    const container = document.querySelector(containerName);    //Get name of container
-    const childElements = container.querySelectorAll('img');    //Get all IMG children in this container
-    const numOfChildren = childElements.length;  //Count how many children there are
+function iconHover(image) {
+    var message;
+    switch(image) {
+        case 'search':
+            message = "I will maximize your websites visibility on search engines like Google to ensure it will appear as high on the search results as possible.";
+            break;
 
-    childElements.forEach((child, index) => {
-        const angle = (index / numOfChildren) * (2 * Math.PI);    //Calculate angle for each child
-    
-    const x = Math.cos(angle) * radius;
-    const y =  Math.sin(angle) * radius;
+        case 'mobile':
+            message = "Your website will be optimzed for a mobile first approach, meaning the experience will perfect on phones";
+            break;
 
-    child.style.transform = `translate(${x}px, ${y}px`;
-    
-    });
+        case 'ui-ux':
+            message = "Many websites struggle to create a seamless and intiutive interface, heavily prioritizing aesthetics. I have a user experiece first appraoch so that no one will struggle to use it.";
+            break;
+
+        case 'support':
+            message = "If there are ever any technical issues with your website, I'm only an email away. I will prioritize solving the issue above all else.";
+            break;
+
+        case 'handshake':
+            message = "Once the website is finished, I will ensure a painless transfer of ownership to you. You'll be able to make adjustments to your website yourself via WordPress.";
+            break;
+    }
+    document.getElementById("icon-desc").innerHTML = message;
 }
